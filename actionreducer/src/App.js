10149faa2e0740b5d708f4a2bd89baef4aa71694
jaswanth geddam <87421953+jash444@users.noreply.github.com>
2022-06-06 15:11:19 +0530

    action-reducer

---
+++ actionreducer/src/App.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+import { useDispatch, useSelector } from "react-redux";
+import { addUserRequest, addUserSuccess } from "./redux/action";
+
+export default function App() {
+ // const dispatch=useDispatch
+
+  //dispatch(addUserRequest({}))
+  // dispatch(addUserSuccess({
+  //   name:"jaswanth",
+  //   id:123,
+  //   title:"hello"
+
+  // }))
+  return (
+    <div className="App">
+    </div>
+  );
+}
---
+++ actionreducer/src/redux/store.js
@@ -0,0 +1,11 @@
+// IGNORE, NO NEED TO WRITE CODE HERE
+
+import reducer from "./reducer"
+import { legacy_createStore } from "redux";
+
+
+export const store=legacy_createStore(reducer);
+
+
+
+
